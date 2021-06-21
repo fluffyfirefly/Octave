be382bb37f12caade7588e20216057e6b9b88e42
Devoxin <lukel9914@gmail.com>
2021-06-21 20:35:38 +0100

    Improve Ignore responses

--- src/main/kotlin/gg/octave/bot/commands/settings/Ignore.kt
+++ src/main/kotlin/gg/octave/bot/commands/settings/Ignore.kt
@@ -24,6 +24,7 @@
 
 package gg.octave.bot.commands.settings
 
+import gg.octave.bot.utils.OctaveBot
 import gg.octave.bot.utils.extensions.DEFAULT_SUBCOMMAND
 import gg.octave.bot.utils.extensions.data
 import me.devoxin.flight.api.Context
@@ -38,70 +39,46 @@ import net.dv8tion.jda.api.entities.Role
 import net.dv8tion.jda.api.entities.TextChannel
 
 class Ignore : Cog {
-    @Command(description = "Configure user/channel/role ignoring.", userPermissions = [Permission.MANAGE_SERVER])
+    @Command(aliases = ["blacklist", "unignore"], description = "Configure user/channel/role ignoring.", userPermissions = [Permission.MANAGE_SERVER])
     fun ignore(ctx: Context) = DEFAULT_SUBCOMMAND(ctx)
 
     @SubCommand(description = "Toggle ignore for a user.")
     fun user(ctx: Context, @Greedy member: Member) {
         val data = ctx.data
-        val ignored = data.ignored
-
-        if (member.id in ignored.users) {
-            ignored.users.remove(member.id)
-            ctx.send("No longer ignoring user ${member.asMention}.")
-        } else {
-            ignored.users.add(member.id)
-            ctx.send("Now ignoring user ${member.asMention}.")
-        }
-
+        val elementAdded = data.ignored.users.toggle(member.id)
         data.save()
+
+        ctx.send {
+            setColor(OctaveBot.PRIMARY_COLOUR)
+            setDescription("${if (elementAdded) "Now" else "No longer"} ignoring user ${member.asMention}.")
+            setFooter("Run the same command again to ${if (elementAdded) "un" else ""}ignore them.")
+        }
     }
 
     @SubCommand(description = "Toggle ignore for a channel.")
     fun channel(ctx: Context, @Greedy channel: TextChannel) {
         val data = ctx.data
-        val ignored = data.ignored
-
-        if (channel.id in ignored.channels) {
-            ignored.channels.remove(channel.id)
-            ctx.send("No longer ignoring channel ${channel.asMention}.")
-        } else {
-            ignored.channels.add(channel.id)
-            ctx.send("Now ignoring channel ${channel.asMention}.")
-        }
-
+        val elementAdded = data.ignored.channels.toggle(channel.id)
         data.save()
+
+        ctx.send {
+            setColor(OctaveBot.PRIMARY_COLOUR)
+            setDescription("${if (elementAdded) "Now" else "No longer"} ignoring channel ${channel.asMention}.")
+            setFooter("Run the same command again to ${if (elementAdded) "un" else ""}ignore them.")
+        }
     }
 
     @SubCommand(description = "Toggle ignore for a role.")
     fun role(ctx: Context, @Greedy role: Role) {
         val data = ctx.data
-        val ignored = data.ignored
-
-        if (role.id in ignored.roles) {
-            ignored.roles.remove(role.id)
-            ctx.send("No longer ignoring role ${role.asMention}.")
-        } else {
-            ignored.roles.add(role.id)
-            ctx.send("Now ignoring role ${role.asMention}.")
-        }
-
+        val elementAdded = data.ignored.roles.toggle(role.id)
         data.save()
-    }
 
-    fun <T : IMentionable> mapper(type: String, data: Set<String>, transform: (String) -> T?): String {
-        return data.mapNotNull(transform)
-            .takeIf { it.isNotEmpty() }
-            ?.map(IMentionable::getAsMention)
-            ?.joinToString("\n") { "• $it" }
-            ?: "No $type are ignored."
-    }
-
-    fun mapString(type: String, data: Set<String>, transform: (String) -> String): String {
-        return data.map(transform)
-            .takeIf { it.isNotEmpty() }
-            ?.joinToString("\n") { "• $it" }
-            ?: "No $type are ignored."
+        ctx.send {
+            setColor(OctaveBot.PRIMARY_COLOUR)
+            setDescription("${if (elementAdded) "Now" else "No longer"} ignoring role ${role.asMention}.")
+            setFooter("Run the same command again to ${if (elementAdded) "un" else ""}ignore them.")
+        }
     }
 
     @SubCommand(description = "Lists all entities that are currently being ignored.")
@@ -115,5 +92,36 @@ class Ignore : Cog {
             addField("Channel", mapper("channels", ignored.channels, ctx.guild!!::getTextChannelById), true)
             addField("Roles", mapper("roles", ignored.roles, ctx.guild!!::getRoleById), true)
         }
+    }
+
+    private fun <T : IMentionable> mapper(type: String, data: Set<String>, transform: (String) -> T?): String {
+        return data.mapNotNull(transform)
+            .takeIf { it.isNotEmpty() }
+            ?.map(IMentionable::getAsMention)
+            ?.joinToString("\n") { "• $it" }
+            ?: "No $type are ignored."
+    }
+
+    private fun mapString(type: String, data: Set<String>, transform: (String) -> String): String {
+        return data.map(transform)
+            .takeIf { it.isNotEmpty() }
+            ?.joinToString("\n") { "• $it" }
+            ?: "No $type are ignored."
+    }
+
+    /**
+     * Toggles an element in a set. If it exists, it's removed. If it doesn't, it's added.
+     * @return True if the element is in the set after toggling.
+     */
+    private fun <T> MutableSet<T>.toggle(e: T): Boolean {
+        val existsInSet = this.contains(e)
+
+        if (existsInSet) {
+            this.remove(e)
+        } else {
+            this.add(e)
+        }
+
+        return !existsInSet
     }
 }
